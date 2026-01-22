8b9f55647fb4c4c9cc0adda34ff49eeb3b924fc6
Thach Nguyen <thachnguyen220805@gmail.com>
2026-01-22 15:19:59 +0700

    Add player movement and initial assets
    player's movement and set up environment

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1.6f, 0);
+
+        // Lấy rotation yaw của player
+        float yaw = player.transform.eulerAngles.y;
+
+        // Nếu muốn thêm pitch offset (ví dụ nhìn xuống 10 độ)
+        float pitch = 10f; 
+
+        // Gán rotation cho camera
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+    [SerializeField] private float rotateSpeed = 5;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    // Player's movement
+    void HandleMovement()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
